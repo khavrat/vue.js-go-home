0cd65386ac4c07a2762982262e0e91ddb0bb025c
khavrat <ihavrat@gmail.com>
2023-08-30 15:44:11 +0300

    created ApartmentsItem component draft

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <ApartmentsItemVue
+      :descr="apartment.descr"
+      :price="apartment.price"
+      :rating="apartment.rating"
+    />
+  </div>
+</template>
+
+<script>
+import ApartmentsItemVue from "./components/apartment/ApartmentsItem";
+
+export default {
+  name: "App",
+  components: {
+    ApartmentsItemVue,
+  },
+  data() {
+    return {
+      apartment: {
+        descr: "qwertyu asdfghj xcvbnm",
+        price: 1010,
+        rating: 3.5,
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  min-height: 100vh;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,49 @@
+<template>
+  <button
+    v-bind:type="type"
+    :class="{btn: true, 'btn--outlined': outlined}"
+    @click="handleClick"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "MainButton",
+  props: {
+    type: {
+      type: String,
+      default: "button",
+    },
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleClick() {
+      this.$emit("customClick");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  display: inline-flex;
+  font-size: 18px;
+  background: #ff662d;
+  color: #fff;
+  cursor: pointer;
+  min-width: 220px;
+  border: 1px solid transparent;
+  padding: 8px 15px;
+
+  &--outlined {
+    background: transparent;
+    color: #ff662d;
+    border: 1px solid #ff662d;
+  }
+}
+</style>
