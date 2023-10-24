ea956ed8184a31b2c4b18464505ad9ae033f962c
khavrat <ihavrat@gmail.com>
2023-10-24 21:09:05 +0300

    added ApartmentPage

---
+++ src/components/apartment/ApartmentsMainInfo.vue
@@ -0,0 +1,91 @@
+<template>
+  <article class="apartment-main-info">
+    <div class="apartment-main-info__heading">
+      <h1 class="apartment-main-info__title">{{ apartment.title }}</h1>
+      <StarRaiting :rating="apartment.rating"/>
+    </div>
+    <img :src="apartment.imgUrl" alt="apartment" class="apartment-main-info__photo" />
+    <p class="apartment-main-info__description">{{ apartment.descr }}</p>
+    <div class="apartment-main-info__btn">
+    </div>
+  </article>
+</template>
+
+<script>
+// import { bookApartment } from '../../services/order.service';
+import StarRaiting from '../StarRating.vue';
+
+export default {
+    name: 'ApartmentsMainInfo',
+    components: {
+    StarRaiting
+  },
+  props: {
+    apartment: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+//   methods: {
+//     async handleApartmentsBooking() {
+//       const body = {
+//         apartmentId: this.$route.params.id,
+//         date: Date.now(),
+//       };
+
+//       try {
+//         this.isLoading = true;
+//         await bookApartment(body);
+
+//         this.$notify({
+//           type: 'success',
+//           title: 'Заказ добавлен',
+//         });
+//       } catch (error) {
+//         this.$notify({
+//           type: 'error',
+//           title: 'Произошла ошибка',
+//           text: error.message,
+//         });
+//       } finally {
+//         this.isLoading = false;
+//       }
+//     },
+//   },
+};
+</script>
+
+<style lang="scss" scoped>
+.apartment-main-info {
+  &__heading {
+    display: flex;
+  }
+
+  &__title {
+    font-size: 20px;
+    font-weight: 700;
+    text-transform: capitalize;
+    margin-bottom: 20px;
+    margin-right: 20px;
+  }
+
+  &__photo {
+    max-width: 100%;
+  }
+
+  &__description {
+    line-height: 1.3;
+    margin-top: 30px;
+  }
+
+  &__btn {
+    margin-top: 20px;
+    text-align: center;
+  }
+}
+</style>
--- src/pages/ApartmentPage.vue
+++ src/pages/ApartmentPage.vue
@@ -1,28 +1,54 @@
 <template>
-  <Container>
-    <h1 class="page">ApartmentPage</h1>
-    <p>Apartment ID: {{ $route.params.id }}</p>
-  </Container>
+  <main  class="apartment-page">
+    <Container>
+      <div class="apartment-page__content">
+        <ApartmentsMainInfo :apartment="apartment" />
+        <ApartmentsOwner :owner="apartment.owner"/>
+      </div>
+    </Container>
+  </main>
 </template>
 
 <script>
 import Container from "../components/shared/Container.vue";
+import apartments from "@/components/apartment/apartments";
+import ApartmentsMainInfo from "../components/apartment/ApartmentsMainInfo.vue";
+import ApartmentsOwner from "../components/apartment/ApartmentsOwner.vue"
 
 export default {
   name: "ApartmentPage",
   components: {
     Container,
+    ApartmentsMainInfo,
+    ApartmentsOwner,
+  },
+  computed: {
+    apartment() {
+      return apartments.find(
+        (apartment) => apartment.id === this.$route.params.id
+      );
+    },
   },
   mounted() {
-    console.log(" this.$router:>> ", this.$router);
-    console.log(" this.$route:>> ", this.$route);
+    console.log("this.apartment", this.apartment);
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.page {
-  font-size: 100px;
-  color: blue;
-}
-</style>
+.apartment-page {
+  padding-top: 120px;
+  padding-bottom: 55px;
+
+  &__content {
+    display: flex;
+    align-items: flex-start;
+  }
+
+  &__additional-info {
+    margin-left: 30px;
+    max-width: 350px;
+    flex-grow: 0;
+    flex-shrink: 1;
+  }
+}</style>
